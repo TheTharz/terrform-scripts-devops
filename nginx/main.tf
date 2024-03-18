e11ba45d70d4f6c6ea063a659a936d969b3dfe01
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-03-18 23:40:54 +0530

    nginx code created but issue with localhost

---
+++ nginx/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+
+}
+
+provider "docker" {
+
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx_container" {
+  image = docker_image.nginx.name
+  name  = "nginx_container"
+  ports {
+    internal = 80
+    external = 8080
+    #if can not access in the localhsot try editing the vagrant file forward port comment
+  }
+  
+}
