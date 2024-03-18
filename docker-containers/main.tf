1c6180be9288eb95320940a140bbeb33f5aefa9c
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-03-18 22:41:45 +0530

    Update docker image and add port mapping

--- docker-containers/main.tf
+++ docker-containers/main.tf
@@ -1,3 +1,27 @@
+# terraform {
+#   required_providers {
+#     docker = {
+#       source  = "kreuzwerker/docker"
+#       version = "3.0.2"
+#     }
+#   }
+# }
+
+# provider "docker" {
+#   host = "unix:///var/run/docker.sock"
+# }
+
+# # Pulls the image
+# resource "docker_image" "ubuntu" {
+#   name = "ubuntu:latest"
+# }
+
+# # Create a container
+# resource "container" "ubuntu_container" {
+#   image = docker_image.ubuntu.image_id
+#   name  = "ubuntu_container"
+# }
+
 terraform {
   required_providers {
     docker = {
@@ -5,19 +29,22 @@ terraform {
       version = "3.0.2"
     }
   }
-}
 
+}
 provider "docker" {
-  host = "unix:///var/run/docker.sock"
+  
 }
 
-# Pulls the image
 resource "docker_image" "ubuntu" {
-  name = "ubuntu:latest"
+  name = "ubuntu:22.04"
+  keep_locally = true
 }
 
-# Create a container
-resource "container" "ubuntu_container" {
+resource "docker_container" "ubuntu_container" {
   image = docker_image.ubuntu.image_id
   name  = "ubuntu_container"
+  ports {
+    internal = 80
+    external = 8080
+  }
 }
